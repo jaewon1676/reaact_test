3bf8dfedae18a1414fa4e66407d5726a2ffe2e2d
jaewon1676 <jaewon1676@daum.net>
2021-11-23 00:03:10 +0900

    react_test_2days

--- src/App.js
+++ src/App.js
@@ -1,85 +1,74 @@
+/* eslint-disable */
 import './App.css';
 import React, {useState} from 'react';
 
 function App() {
 
-    let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+    let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '판교 원룸']);
     let [따봉, 따봉변경] = useState(0);
     let [modal, modal변경] = useState(false);
     let posts = '강남 고기 맛집';
     let [누른제목, 누른제목변경] = useState(0);
     let [입력값, 입력값변경] = useState('');
 
-    function 반복된UI(){
-        
-        var array = []
-        for (let i=0; i<3; i++){
-            array.push(<div>안녕</div>)
-        }
-        
-        return array
+
+    function 코트변경(){
+        var array = [...글제목];
+        array[0] = '여자 코트 추천';
+        글제목변경(array);
     }
 
-
-    function 제목바꾸기() {
-        var newArray = [...글제목];
-        newArray[0] = ['여자 코트 추천']
-        글제목변경(newArray);
-    }
-
-
-    return (
-        <div className="App">
+    return(
+        <div className='app'>
             <div className='black-nav'>
                 <div>개발 Blog</div>
             </div>
-            <button onClick={제목바꾸기}>버튼!</button>
-
             
+            <button onClick={코트변경}>코트변경</button>
+
             {
-                글제목.map(function (n, i) {
+                글제목.map(function(a, i){
                     return (
-                        <div className='list' key={i}>
-                            <h3 onClick={ () => { 누른제목변경(i) }} >
-                        <span onClick={ () => {따봉변경(따봉+1)}  }>{n} ★{따봉}</span> </h3>
-                            <p>2월 17일 발행</p>
-                            <hr/>
-                        </div>
-                    )
+                    <div className='list'>
+                    <h4 onClick = { () => {누른제목변경(i)}}>{ a }</h4>
+                    <p>2월 17일 발행<span onClick={ () => {따봉변경(따봉 + 1)}}> Θ </span>{따봉}</p>
+                    <hr/>
+                    </div>)
                 })
             }
 
+            
+        {/* <button onClick={ () => {누른제목변경(누른제목 = 0)}}>버튼1</button>
+            <button onClick={ () => {누른제목변경(누른제목 = 1)}}>버튼2</button>
+            <button onClick={ () => {누른제목변경(누른제목 = 2)}}>버튼3</button> */}
 
+
+    
             <div className='publish'>
-                <input onChange = { (e) => {입력값변경(e.target.value)} }/>
-                <button onClick = { () => {
+                <input onChange={(e) => {입력값변경(e.target.value)}} />
+                <button onClick = {() => {
                     var array = [...글제목];
                     array.unshift(입력값);
                     글제목변경(array);
-                }}>저장</button>
+                    }}>저장</button>
             </div>
 
-
-            <button onClick = { () => { modal변경(!modal)}}>모달창 열고닫기</button>
-            
-            {
-                modal === true
-                    ? <Modal 글제목 = {글제목} 누른제목 = {누른제목}></Modal>
-                    : null
-            }
-            
-            
+           <button onClick={() => {modal변경(!modal)}}>모달</button>
+           {
+               modal === true ? <Modal 글제목 = {글제목} 누른제목 = {누른제목}/> : null
+           }
         </div>
     );
-
 }
 
-function Modal(props) {
-    return (
-        <div className='modal'>
-            <h3>{props.글제목[props.누른제목]}</h3>
+function Modal(props){
+    return(
+        <div>
+            <div className='modal'>
+            <h2>{props.글제목[props.누른제목]}</h2>
             <p>날짜</p>
             <p>상세내용</p>
+            </div>
         </div>
     )
 }
